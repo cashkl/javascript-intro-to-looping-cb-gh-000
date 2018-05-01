1ce1e7fb3e7fb943be9671491d98f34a9c0e4d18
Murat Celik <cashkl@yandex.com>
2018-05-01 11:15:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    array[array.length]=`I am ${i} strange loop`
+    array.push(`I am ${i} strange loop`)
   }
 }
 function whileLoop(i != 0) {
